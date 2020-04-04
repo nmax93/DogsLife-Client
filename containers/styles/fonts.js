6c425851db18b4fc7fae3c7572bdc06fe000b539
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-04-04 15:53:16 +0300

    add px units

--- containers/styles/fonts.js
+++ containers/styles/fonts.js
@@ -14,16 +14,16 @@ export const BigText = styled(Text)`
     font-size: 
 `;
 export const MedBoldText = styled(Text)`
-    font-size: ${DIMENSION_HEIGHT*0.024}
+    font-size: ${DIMENSION_HEIGHT*0.024}px
     font-weight: bold;
 `;
 export const MedText = styled(Text)`
-    font-size: ${DIMENSION_HEIGHT*0.024}
+    font-size: ${DIMENSION_HEIGHT*0.024}px
 `;
 export const SmallBoldText = styled(Text)`
-    font-size: ${DIMENSION_HEIGHT*0.021}
+    font-size: ${DIMENSION_HEIGHT*0.021}px
     font-width: bold; 
 `;
 export const SmallText = styled(Text)`
-    font-size: ${DIMENSION_HEIGHT*0.021}
+    font-size: ${DIMENSION_HEIGHT*0.021}px
 `;
