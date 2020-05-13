3badc7887f0e7200ea8837345e14e9abdae5692a
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-05-13 15:58:43 +0300

    ignoreWarnings

--- index.js
+++ index.js
@@ -4,15 +4,18 @@
 import React, { Component } from 'react'
 import { Provider } from 'mobx-react'
 import {AppRegistry} from 'react-native';
+import { YellowBox } from 'react-native'
 import App from './App';
 import {name as appName} from './app.json';
 import rootStore from './provider/rootStore';
+import BleController from './bleManager/index2';
 
 class Application extends Component {
     render() {
       return (
         <Provider rootStore={rootStore}>
             <App />
+            <BleController />
         </Provider>
       )
     }
@@ -20,3 +23,9 @@ class Application extends Component {
 
 
 AppRegistry.registerComponent(appName, () => Application);
+
+
+YellowBox.ignoreWarnings([
+  'VirtualizedLists should never be nested', // TODO: Remove when fixed
+  'componentWillReceiveProps has been renamed, and is not recommended for use.'
+])
