892a5ac36d06f2af31cbed9c466404e691b90d2c
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-05-13 15:43:40 +0300

    serverUrl

--- containers/registerScreen/store/routes.js
+++ containers/registerScreen/store/routes.js
@@ -1,7 +1,7 @@
 
 import { consts } from '../../../consts';
 export async function registerRequest(data) {    
-    const response = await fetch(`${consts.LocalHostUrl}/register`, {
+    const response = await fetch(`${consts.serverUrl}/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
