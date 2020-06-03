5f715a7f8825f69f39228450a5f8de99cc3bc4c6
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-06-03 11:11:39 +0300

    updated colors

--- containers/styles/colors.js
+++ containers/styles/colors.js
@@ -4,4 +4,6 @@ export const BLACK = '#000000';
 export const DARK_GRAY = '#333333';
 export const LIGHT_GREEN = '#c8d9cb';
 export const LIGHT_PURPLE = '#d4c8d1';
+export const LIGHT_RED = '#e07583';
 export const LIGHT_BROWN = '#8e5b5e';
+export const CORGI_COLOR = '#f5d8af';
