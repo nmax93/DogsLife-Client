4c086050df3ac941f883c8c1fb76368b4a2222a0
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-06-03 14:23:33 +0300

    dog info profile info

--- components/ProfileItem.js
+++ components/ProfileItem.js
@@ -1,49 +1,84 @@
 import React from 'react';
 import styles from './styles/ProfileItemStyle';
 import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Bolt from 'react-native-vector-icons/FontAwesome';
+import IconGender from 'react-native-vector-icons/Ionicons';
+import { BigBoldText } from '../containers/styles/fonts';
+
+
+
 
 const ProfileItem = ({
-  age,
-  info1,
-  info2,
-  info3,
-  info4,
-  location,
-  matches,
   name,
+  age,
+  breed,
+  mix,
+  description,
+  gender,
+  spayed,
+  playfullness,
+  parkPreference,
 }) => {
   return (
     <View style={styles.containerProfileItem}>
-      <Text style={styles.name}>{name}</Text>
-
+      <BigBoldText style={styles.name}>{name}</BigBoldText>
       <Text style={styles.descriptionProfileItem}>
-        {age} - {location}
+        {age}
       </Text>
-
       <View style={styles.info}>
-        <Text style={styles.iconProfile}>
-        </Text>
-        <Text style={styles.infoContent}>{info1}</Text>
+      <View style={styles.iconView}>
+        <Icon name="dog" size={20} color="black" style={styles.iconStyle} />
+        </View>        
+        <Text style={styles.infoContent}>{breed}  </Text>
+        {mix ? <Text>Mix</Text> : null}
       </View>
 
       <View style={styles.info}>
-        <Text style={styles.iconProfile}>
-        </Text>
-        <Text style={styles.infoContent}>{info2}</Text>
+        <View style={styles.iconView}>
+      <Icon name="info" size={20} color="black" style={styles.iconStyle} />
+        </View>
+        <Text style={styles.infoContent}>{description}</Text>
       </View>
 
       <View style={styles.info}>
-        <Text style={styles.iconProfile}>
-        </Text>
-        <Text style={styles.infoContent}>{info3}</Text>
+        <View style={styles.iconView}>
+        {gender == 1 ? (
+          <IconGender
+          name="md-male"
+          size={20}
+          color={'black'}
+          style={styles.iconStyle}
+        />
+      ) : (
+        <IconGender
+        name="md-female"
+        size={20}
+        color={'black'}
+        style={styles.iconStyle}
+      />
+      )}
+        </View>
+        <Text style={styles.infoContent}>{gender == 1 ? `Male` : `Female`}</Text>
+        {spayed ? <Text style={styles.infoContent}>spayed</Text> : <Text style={styles.infoContent}>not spayed</Text>}
       </View>
 
       <View style={styles.info}>
-        <Text style={styles.iconProfile}>
-        </Text>
-        <Text style={styles.infoContent}>{info4}</Text>
+        <View style={styles.iconView}>
+      <Bolt name="bolt" size={20} color="black" style={styles.iconStyle} />
+        </View>
+        <Text style={styles.infoContent}>{playfullness ? `Playful` : `Inactive`}</Text>
       </View>
+
+      {parkPreference !=3 ? <View style={styles.info}>
+        <View style={styles.iconView}>
+      <Bolt name="tree" size={20} color="black" style={styles.iconStyle} />
+        </View>
+        <Text style={styles.infoContent}>{parkPreference==1 ? `Prefer Crowded park` : `Prefer not crowded park`}</Text>
+      </View>: null}
     </View>
+
+    
   );
 };
 
