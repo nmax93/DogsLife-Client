2a03d0e2a7d414a01459fdea73e75412721d8c81
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-07-02 19:57:25 +0300

    resetObservables StoreDogThirdScreen

--- containers/signUpProcess/dogInfo/dogThirdScreen/store/index.js
+++ containers/signUpProcess/dogInfo/dogThirdScreen/store/index.js
@@ -1,66 +1,79 @@
 import {observable, action, flow} from 'mobx';
 
 export class StoreDogThirdScreen {
-    constructor(rootStore) {
-        this.rootStore = rootStore;
-      }
-    
+  constructor(rootStore) {
+    this.rootStore = rootStore;
+  }
 
-      @observable energyLevel = 1;
-      @observable getAlongGender = 3;
-      @observable getAlongSpayed = 3;
-      @observable getAlongSize = 3;
-      @observable player = true;
-      @observable parkPre = 1;
+  @observable energyLevel = 1;
+  @observable getAlongGender = 3;
+  @observable getAlongSpayed = 3;
+  @observable getAlongSize = 3;
+  @observable player = true;
+  @observable parkPre = 1;
 
+  @observable errorMsg = null;
+  @observable loading = false;
 
+  @observable signupObject = {
+    energyLevel: 1,
+    getAlongGender: 3,
+    getAlongSpayed: 3,
+    getAlongSize: 3,
+    player: true,
+    parkPre: 1,
+  };
 
-      @observable errorMsg = null;
-      @observable loading = false;
+  @action
+  resetObservables() {
+    console.log('reset third dog  called');
+    this.energyLevel = 1;
+    this.getAlongGender = 3;
+    this.getAlongSpayed = 3;
+    this.getAlongSize = 3;
+    this.player = true;
+    this.parkPre = 1;
+    this.errorMsg = null;
 
-      @observable signupObject = {
-                                    energyLevel: 1,
-                                    getAlongGender: 3,
-                                    getAlongSpayed: 3,
-                                    getAlongSize: 3,
-                                    player: true,
-                                    parkPre: 1
-                                }
+    this.signupObject = {
+      energyLevel: 1,
+      getAlongGender: 3,
+      getAlongSpayed: 3,
+      getAlongSize: 3,
+      player: true,
+      parkPre: 1,
+    };
+  }
+  @action
+  setEnergyLevel(selection) {
+    this.energyLevel = selection;
+    this.signupObject.energyLevel = this.selection;
+  }
 
-    
+  @action
+  setGetAlongGender(selection) {
+    this.getAlongGender = selection;
+    this.signupObject.getAlongGender = this.getAlongGender;
+  }
+  @action
+  setGetAlongSpayed(selection) {
+    this.getAlongSpayed = selection;
+    this.signupObject.getAlongSpayed = this.getAlongSpayed;
+  }
+  @action
+  setGetAlongSize(selection) {
+    this.getAlongSize = selection;
+    this.signupObject.getAlongSize = this.getAlongSize;
+  }
 
-      @action
-      setEnergyLevel(selection) {
-        this.energyLevel = selection;
-        this.signupObject.energyLevel = this.selection;
-      }
-
-      @action
-      setGetAlongGender(selection){
-        this.getAlongGender = selection;
-        this.signupObject.getAlongGender = this.getAlongGender;
-      }
-      @action
-      setGetAlongSpayed(selection){
-        this.getAlongSpayed = selection;
-        this.signupObject.getAlongSpayed = this.getAlongSpayed;
-      }
-      @action
-      setGetAlongSize(selection){
-        this.getAlongSize = selection;
-        this.signupObject.getAlongSize = this.getAlongSize;
-      }
-
-      @action
-      setPlayer() {
-        this.player = !this.player;
-        this.signupObject.player = this.player;
-     }
-     @action
-     setParkPreference(value) {
-       this.parkPre = value;
-       this.signupObject.parkPre = this.parkPre;
-
-    }
+  @action
+  setPlayer() {
+    this.player = !this.player;
+    this.signupObject.player = this.player;
+  }
+  @action
+  setParkPreference(value) {
+    this.parkPre = value;
+    this.signupObject.parkPre = this.parkPre;
+  }
 }
-
