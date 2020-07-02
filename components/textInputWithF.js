ef4138895c1fe7cd7254dc173003b997d7b5903d
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-07-02 20:12:18 +0300

    add value to textinput

--- components/textInputWithF.js
+++ components/textInputWithF.js
@@ -14,6 +14,7 @@ export class TextInputWithF extends PureComponent {
         style={[styles.textInput, this.props.s]}
         onChangeText={text => this.props.onChange(text)}
         placeholder={this.props.placeholder}
+        value={this.props.value}
       />
     );
   }
