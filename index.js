3e4d4f7bd36e9e503574c564ba297be302a2d4ee
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-07-02 18:37:57 +0300

    remove BleController

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ import { YellowBox } from 'react-native'
 import App from './App';
 import {name as appName} from './app.json';
 import rootStore from './provider/rootStore';
-import BleController from './bleManager/index2';
 
 class Application extends Component {
     render() {
       return (
         <Provider rootStore={rootStore}>
             <App />
-            {/* <BleController /> */}
         </Provider>
       )
     }
