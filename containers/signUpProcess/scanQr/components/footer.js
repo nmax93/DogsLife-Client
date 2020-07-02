5e252a36e7202cfb3014f6b1cb0e6ff03f6ee430
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-07-02 19:53:09 +0300

    scan footer

--- containers/signUpProcess/scanQr/components/footer.js
+++ containers/signUpProcess/scanQr/components/footer.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import {StyleSheet, Dimensions, View} from 'react-native';
-import {WhiteButton} from '../../components/whiteButton';
+import {WhiteButton} from './whiteButton';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
-    width: width,
+    width: width * 0.88,
     alignItems: 'center',
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+    flexDirection: 'row-reverse',
     position: 'absolute',
-    bottom: height*0.05
+    bottom: height*0.05,
   },
 });
 
 export const Footer = props => {
   return (
     <View style={styles.container}>
-        <WhiteButton onPress={props.onPress} navigation={props.navigation} text={props.text} next={props.next}/>
+        {props.isDone && (!props.twoDogs || props.onSecondDog)  ? <WhiteButton onPress={props.doneFunc} text={`Done!`} /> : null}
+        {props.checkIdInServer ? <WhiteButton onPress={props.onPressDoneEnterId} text={`Done!`} /> : null}
+        {props.isDone && (props.twoDogs && !props.onSecondDog) ? <WhiteButton onPress={props.secondDogFunc} text={`Second dog`}/> : null}
+        {props.noCollar && !props.isDone ? <WhiteButton onPress={props.noCollarFunc} noCollar text={`I don't have a collar`}  /> : null}
     </View>
   );
 };
